54598f84eb3c28304ce86e6417ee65bb29497f59
TEX-2 pc <pshcyrill@mail.ru>
2019-06-10 17:20:10 +0300

    Mutex!

--- SerialPort.cpp
+++ SerialPort.cpp
@@ -3,10 +3,14 @@
 //
 
 #include "SerialPort.h"
+#include <iostream>
+#include <mutex>
 
 using namespace SP;
 
 bool SerialPort::busy = false;
+
+std::mutex mux;
 
 SerialPort::SerialPort(const char *path) {
     sp = new boost::asio::serial_port(ios,path);
@@ -19,23 +23,39 @@ SerialPort::~SerialPort() {
 
 /* method for readwrite to controller */
 void SerialPort::readwrite(char *req, char *res, size_t *size_req, size_t *size_res){
+    //while(busy==true);
+    //busy = true;
+
+    mux.lock();
     // just sleep for wait device
     struct timespec ts;
     ts.tv_sec = 0;
     ts.tv_nsec = 200000000;  //200 ms delay
     nanosleep(&ts,NULL);
 
-    while(busy);
-    busy = true;
+    std::cout << "send data\n";
+
     // request, return the number of write bytes
     if(*size_req!=0) *size_req = sp->write_some(boost::asio::buffer(req, *size_req));
+    //if(*size_req!=0) sp->async_write_some(boost::asio::buffer(req, *size_req),handler);
     // responsabal, return the number of read bytes
     char tmp = '\x00';
+    //sp->async_read_some(boost::asio::buffer(res, *size_res),handler);
+    //sp->read_some(boost::asio::buffer(res, *size_res));
+
+
     for(int i=0;i<*size_res;i++){
         if(sp->read_some(boost::asio::buffer(&tmp, 1))<=0) break;
+        //sp->async_read_some(boost::asio::buffer(&tmp, 1),handler);
         res[i] = tmp;
     }
-    busy = false;
+    mux.unlock();
+}
+
+
+void SerialPort::handler(const boost::system::error_code &error, std::size_t bytes_transferred){
+    std::cout << "bytes: " << bytes_transferred;
+    return;
 }
 
 /* methods for set and get private baud_rate */
